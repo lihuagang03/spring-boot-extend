86a9ba8d3ee82c7c2ee519f0ab3ba430182b67fc
bert82503 <bert825_work@163.com>
2023-07-21 23:37:36 +0800

    add ExampleNacosConfigListener

---
+++ spring-boot-starters-example/nacos-config-spring-boot-starter-example/src/main/java/com/spring/boot/nacos/example/config/ExampleNacosConfigListener.java
@@ -0,0 +1,43 @@
+package com.spring.boot.nacos.example.config;
+
+import com.alibaba.nacos.api.config.ConfigType;
+import com.alibaba.nacos.api.config.annotation.NacosConfigListener;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置属性
+ *
+ * @author guangyi
+ * @date 2023/7/21
+ * @see NacosValue
+ * @see NacosConfigListener
+ */
+@Slf4j
+@Data
+@Configuration(value = "exampleNacosConfigListener", proxyBeanMethods = false)
+public class ExampleNacosConfigListener {
+
+    /**
+     * 配置属性值
+     */
+    @NacosValue(value = "${people.count:0}", autoRefreshed = true)
+    private String count;
+
+    /**
+     * 配置监视器
+     *
+     * @see NacosConfigListener
+     */
+    @NacosConfigListener(
+            groupId = "${nacos.config.group:}",
+            dataId = "${nacos.config.data-id:}",
+            type = ConfigType.YAML,
+            timeout = 3_000L
+    )
+    public final void onChange(String newContent) {
+        log.info("config onChange,newContent={}", newContent);
+    }
+}
