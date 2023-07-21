d407011b95d658acf5863ec8e66a473437afe86d
bert82503 <bert825_work@163.com>
2023-07-21 22:12:48 +0800

    improve ConfigServiceDemo

--- spring-boot-starters-example/nacos-config-spring-boot-starter-example/src/main/java/com/spring/boot/nacos/example/service/ConfigServiceDemo.java
+++ spring-boot-starters-example/nacos-config-spring-boot-starter-example/src/main/java/com/spring/boot/nacos/example/service/ConfigServiceDemo.java
@@ -3,6 +3,8 @@ package com.spring.boot.nacos.example.service;
 import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.exception.NacosException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,11 +13,20 @@ import org.springframework.stereotype.Service;
  * @author guangyi
  * @date 2023/7/19
  */
+@Slf4j
 @Service("configServiceDemo")
 public class ConfigServiceDemo {
 
+    /**
+     * 配置服务
+     */
     @NacosInjected
     private ConfigService configService;
+
+    @Value("${nacos.config.group}")
+    private String group;
+    @Value("${nacos.config.data-id}")
+    private String dataId;
 
     // 构造器方法自动注入，不可行
 //    private final ConfigService configService;
@@ -28,14 +39,12 @@ public class ConfigServiceDemo {
 //    }
 
     public String getConfig() {
-        String dataId = "nacos-config-spring-boot-starter-example";
-        String group = "DEFAULT_GROUP";
         try {
             String content = configService.getConfig(dataId, group, 5_000L);
-            System.out.println(content);
+            log.info("get config, content={}", content);
             return content;
         } catch (NacosException e) {
-            e.printStackTrace();
+            log.error("get config content fail,dataId={},group={}", dataId, group, e);
         }
         return null;
     }
