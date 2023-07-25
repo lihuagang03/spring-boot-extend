915cbd3e07f99378fb85357d14e9ac813e22b3ed
bert82503 <bert825_work@163.com>
2023-07-25 22:49:38 +0800

    add Flux.subscribe() examples in FluxTest

--- spring-boot-starters-example/reactive-spring-boot-starter-example/src/test/java/com/spring/boot/reactive/example/FluxTest.java
+++ spring-boot-starters-example/reactive-spring-boot-starter-example/src/test/java/com/spring/boot/reactive/example/FluxTest.java
@@ -1,8 +1,10 @@
 package com.spring.boot.reactive.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 /**
@@ -13,6 +15,9 @@ import reactor.test.StepVerifier;
  */
 class FluxTest {
 
+    /**
+     * 4.3. Simple Ways to Create a Flux or Mono and Subscribe to It
+     */
     @Test
     void createFlux() {
         Flux<String> seq = Flux.just("foo", "bar", "foobar");
@@ -24,6 +29,54 @@ class FluxTest {
                 .expectComplete()
                 .verify()
         ;
+
+        List<String> iterable = Arrays.asList("foo", "bar", "foobar");
+        seq = Flux.fromIterable(iterable);
+
+        StepVerifier.create(seq)
+                .expectNext("foo")
+                .expectNext("bar")
+                .expectNext("foobar")
+                .expectComplete()
+                .verify()
+        ;
+
+        Flux<Integer> numbersFromFiveToSeven = Flux.range(5, 3);
+        StepVerifier.create(numbersFromFiveToSeven)
+                .expectNext(5, 6, 7)
+                .expectComplete()
+                .verify()
+        ;
+    }
+
+    /**
+     * 4.3.1. subscribe Method Examples
+     */
+    @Test
+    void subscribe() {
+        Flux<Integer> integers = Flux.range(1, 3);
+        // The preceding code produces no visible output, but it does work.
+        // The Flux produces three values.
+        integers.subscribe();
+        // If we provide a lambda, we can make the values visible.
+        integers.subscribe(System.out::println);
+
+        // To demonstrate the next signature, we intentionally introduce an error
+        integers = Flux.range(1, 4)
+                .map(integer -> {
+                    if (integer <= 3) {
+                        return integer;
+                    }
+                    throw new RuntimeException("Got to 4");
+                });
+        integers.subscribe(System.out::println,
+                error -> System.err.println("Error: " + error));
+
+        // The next signature of the subscribe method includes both an error handler and a handler for completion events
+        integers = Flux.range(1, 4);
+        integers.subscribe(System.out::println,
+                error -> System.err.println("Error: " + error),
+                () -> System.out.println("Done"));
     }
 
     @Test
@@ -43,6 +96,6 @@ class FluxTest {
      * https://projectreactor.io/docs/core/release/reference/index.html#_testing_a_scenario_with_stepverifier
      */
     private static <T> Flux<T> appendBoomError(Flux<T> source) {
-        return source.concatWith(Mono.error(new IllegalArgumentException("boom")));
+        return source.concatWith(Flux.error(new IllegalArgumentException("boom")));
     }
 }
