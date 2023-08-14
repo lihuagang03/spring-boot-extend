3906a7546a451824c54a10a3a3870126885ff0fa
bert82503 <bert825_work@163.com>
2023-08-14 10:54:13 +0800

    add GreetingRouterTest

---
+++ spring-boot-starters-example/reactive-spring-boot-starter-example/src/test/java/com/spring/boot/reactive/web/flux/GreetingRouterTest.java
@@ -0,0 +1,52 @@
+package com.spring.boot.reactive.web.flux;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.spring.boot.reactive.web.flux.entity.Greeting;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Test of {@link GreetingRouter}.
+ * <p>
+ * We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
+ *
+ * @author guang.yi
+ * @since 2023/8/14
+ * @see WebTestClient
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GreetingRouterTest {
+
+    /**
+     * Spring Boot will create a `WebTestClient` for you,
+     * already configure and ready to issue requests against "localhost:RANDOM_PORT"
+     */
+    @Resource
+    private WebTestClient webTestClient;
+
+    @Test
+    void hello() {
+        // Create a GET request to test an endpoint
+        webTestClient.get()
+                .uri("/hello")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                // and use the dedicated DSL to test assertions against the response
+                .expectStatus()
+                .isOk()
+                .expectBody(Greeting.class)
+                .value(greeting -> assertThat(greeting.getMessage())
+                        .isEqualTo("Hello, Spring!")
+                )
+        ;
+    }
+}
