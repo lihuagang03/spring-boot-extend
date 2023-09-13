ed57917947451566e51552dccdca15ce7c77b3ab
bert82503 <bert825_work@163.com>
2023-09-13 11:33:04 +0800

    Add LivenessStateEventListener and move ReadinessStateEventListener

--- spring-boot-starters-extend/log-spring-boot-starter/src/main/java/com/spring/boot/log/autoconfigure/LogAutoConfiguration.java
+++ spring-boot-starters-extend/log-spring-boot-starter/src/main/java/com/spring/boot/log/autoconfigure/LogAutoConfiguration.java
@@ -2,11 +2,14 @@ package com.spring.boot.log.autoconfigure;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 
+import com.spring.boot.log.LocalCacheVerifier;
 import com.spring.boot.log.LogApplicationRunner;
 import com.spring.boot.log.LogCommandLineRunner;
-import com.spring.boot.log.ReadinessStateEventListener;
+import com.spring.boot.log.event.LivenessStateEventListener;
+import com.spring.boot.log.event.ReadinessStateEventListener;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -39,6 +42,20 @@ public class LogAutoConfiguration {
     @ConditionalOnMissingBean
     public ReadinessStateEventListener readinessStateEventListener() {
         return new ReadinessStateEventListener();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LivenessStateEventListener livenessStateEventListener() {
+        return new LivenessStateEventListener();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LocalCacheVerifier localCacheVerifier(
+            ApplicationEventPublisher applicationEventPublisher
+    ) {
+        return new LocalCacheVerifier(applicationEventPublisher);
     }
 
 }
--- spring-boot-starters-extend/log-spring-boot-starter/src/main/java/com/spring/boot/log/event/LivenessStateEventListener.java
+++ spring-boot-starters-extend/log-spring-boot-starter/src/main/java/com/spring/boot/log/event/LivenessStateEventListener.java
@@ -1,17 +1,20 @@
-package com.spring.boot.log;
+package com.spring.boot.log.event;
 
 import org.springframework.boot.availability.AvailabilityChangeEvent;
-import org.springframework.boot.availability.ReadinessState;
+import org.springframework.boot.availability.LivenessState;
 import org.springframework.context.event.EventListener;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Readiness State of Kubernetes Probes.
+ * Liveness State of Kubernetes Probes.
  * <p>
+ * 7.1.6. Application Availability
  * <a href="https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/">
  *     Kubernetes Probes</a>
  * Spring Boot includes out-of-the box support for the commonly used “liveness” and “readiness” availability states.
+ * <p>
+ * Managing the Application Availability State
  *
  * @author guang.yi
  * @since 2023/9/12
@@ -20,20 +23,20 @@ import lombok.extern.slf4j.Slf4j;
  * @see org.springframework.boot.availability.LivenessState
  */
 @Slf4j
-public class ReadinessStateEventListener {
+public class LivenessStateEventListener {
 
-    public ReadinessStateEventListener() {
-        log.info("create ReadinessStateEventListener");
+    public LivenessStateEventListener() {
+        log.info("create LivenessStateEventListener");
     }
 
     @EventListener
-    public void onStateChangeEvent(AvailabilityChangeEvent<ReadinessState> event) {
+    public void onStateChangeEvent(AvailabilityChangeEvent<LivenessState> event) {
         switch (event.getState()) {
-            case ACCEPTING_TRAFFIC:
-                log.info("receive event ACCEPTING_TRAFFIC");
+            case CORRECT:
+                log.info("receive event CORRECT");
                 break;
-            case REFUSING_TRAFFIC:
-                log.info("receive event REFUSING_TRAFFIC");
+            case BROKEN:
+                log.info("receive event BROKEN");
                 break;
             default:
                 log.info("receive event default");
