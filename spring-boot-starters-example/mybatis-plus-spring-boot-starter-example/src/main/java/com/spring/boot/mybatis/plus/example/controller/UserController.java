ebb496be951f00e58f04a26deec7d4c8fea70c9e
bert82503 <bert825_work@163.com>
2024-11-03 02:54:11 +0800

    Upgrade version spring-boot-starter-parent 3.2.11

--- spring-boot-starters-example/mybatis-plus-spring-boot-starter-example/src/main/java/com/spring/boot/mybatis/plus/example/controller/UserController.java
+++ spring-boot-starters-example/mybatis-plus-spring-boot-starter-example/src/main/java/com/spring/boot/mybatis/plus/example/controller/UserController.java
@@ -1,9 +1,8 @@
 package com.spring.boot.mybatis.plus.example.controller;
 
-import javax.annotation.Resource;
-
 import java.util.List;
 
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
